8385d1bfd1c6da5baa4d220afde800bce2d63f58
nguyen khanh <nguyenkhanh7493@gmail.com>
2018-11-16 16:38:06 +0700

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->string('phone');
+            $table->tinyInteger('gender');
+            $table->string('avatar');
+            $table->integer('status');
+            $table->integer('level');
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
---
+++ resources/views/layouts/frontend/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: N2K
+ * Date: 11/16/2018
+ * Time: 3:36 PM
+ */
