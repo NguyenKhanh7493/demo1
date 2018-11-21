0f2e1fb8f10bfb7898a2b7af69a90077a855b402
NguyenKhanh7493 <nguyenkhanh7493@gmail.com>
2018-11-21 21:32:44 +0700

    12

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,20 +27,20 @@ class HomeController extends Controller
         return view('home');
     }
 
-//    public function insertUser()
-//    {
-//        $user = new User;
-//
-//        $user->name = 'Duong';
-//        $user->fullname = 'NguyenDuong';
-//        $user->address = 'Duong';
-//        $user->phone = '123456789';
-//        $user->gender = '1';
-//        $user->avatar = 'Duong';
-//        $user->status = '1';
-//        $user->level = '1';
-//        $user->email = 'nguyenduong';
-//        $user->password = Hash::make('123456');
-//        $user->save();
-//    }
+   // public function insertUser()
+   // {
+   //     $user = new User;
+
+   //     $user->name = 'Xuân Hiếu';
+   //     $user->fullname = 'Đoàn THị Xuân Hiếu';
+   //     $user->address = 'QUẢNG TRỊ';
+   //     $user->phone = '0972024098';
+   //     $user->gender = '1';
+   //     $user->avatar = 'hieu';
+   //     $user->status = '1';
+   //     $user->level = '1';
+   //     $user->email = 'xuanhieu7496@gmail.com';
+   //     $user->password = Hash::make('123456');
+   //     $user->save();
+   // }
 }
