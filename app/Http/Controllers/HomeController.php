6dc9ff43064af0df56fe1e76a00f50ff88a990aa
nguyen khanh <nguyenkhanh7493@gmail.com>
2019-04-05 11:05:31 +0700

    s

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,6 +45,7 @@ class HomeController extends Controller
 //        echo "<pre>";
 //        print_r($count_ip);
 //        echo "</pre>";die();
+        
         $banner = Banner::where('banner_center',1)->where('status',1)->orderBy('id','DESC')->limit(3)->select('name','title','sort')->get();
         $banner_right = Banner::where('banner_right',1)->where('status',1)->orderBy('id','DESC')->limit(1)->select('name','title','sort')->get();
         $banner_bottom = Banner::where('banner_bottom',1)->where('status',1)->orderBy('id','DESC')->limit(2)->select('name','title','sort')->get();
